dd278beff447a30eb9c62b9d39a8d5176f188d49
GM347El <arsalan.akorn@gmail.com>
2024-11-01 17:01:08 +0330

    second

--- src/DriverDistributor.Shared/obj/Debug/net9.0/DriverDistributor.Shared.AssemblyInfo.cs
+++ src/DriverDistributor.Shared/obj/Debug/net9.0/DriverDistributor.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DriverDistributor.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeeedc568bad0685cd9231ef0c022f2f5cb379cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("DriverDistributor.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DriverDistributor.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
