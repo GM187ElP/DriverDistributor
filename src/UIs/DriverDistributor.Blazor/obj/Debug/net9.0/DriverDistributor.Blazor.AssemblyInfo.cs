8218bc5571bf5de03f157d015dc5f439599935f0
GM347El <arsalan.akorn@gmail.com>
2024-12-08 16:42:35 +0330

    last time

--- src/UIs/DriverDistributor.Blazor/obj/Debug/net9.0/DriverDistributor.Blazor.AssemblyInfo.cs
+++ src/UIs/DriverDistributor.Blazor/obj/Debug/net9.0/DriverDistributor.Blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DriverDistributor.Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3465d08316cb5b0cf399a6134be48576eec90a45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04721fb6fabbe8c046893050e050f157f91aa83b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DriverDistributor.Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DriverDistributor.Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
